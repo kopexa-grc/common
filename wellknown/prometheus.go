4321c9699d713ebfe7222b9ca6a7f932b14bfc8f
Julian Koehn <me@julian.pro>
2025-05-12 00:37:55 +0200

    feat(wellknown): add Prometheus namespace constant and documentation
    - Introduced `PrometheusNamespaceKopexa` constant for standardized Prometheus metrics.
    - Created README.md for the `wellknown` package, detailing usage and best practices for constants.

---
+++ wellknown/prometheus.go
@@ -0,0 +1,7 @@
+// Copyright (c) Kopexa GmbH
+// SPDX-License-Identifier: BUSL-1.1
+
+package wellknown
+
+// PrometheusNamespaceKopexa is the name of the prometheus namespace all metrics created by this library should be part of
+const PrometheusNamespaceKopexa = "kopexa"
